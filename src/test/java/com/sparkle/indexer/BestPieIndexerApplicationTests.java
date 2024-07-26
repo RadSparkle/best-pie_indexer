fef5344690404d22975d23ee30c0f566fbf0b045
RadSparkle <wwwartg1@naver.com>
2024-07-27 00:12:35 +0900

    Initial commit

---
+++ src/test/java/com/sparkle/indexer/BestPieIndexerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparkle.indexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPieIndexerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
